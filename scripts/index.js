3119981190b740b0a5d06159c5bf987be7fb927a
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-01-09 00:15:06 +0300

    2.0

--- scripts/index.js
+++ scripts/index.js
@@ -3,28 +3,37 @@ const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
-const cardTitle = cardTemplate.content.querySelector('.card__title');
-const cardImage = cardTemplate.content.querySelector('.card__image');
 
 // @todo: Функция создания карточки
-const createCard = (obj) => {
+const createCard = (obj, funcRemove) => {
+  // 1. Получение данных из DOM
+  const cardTitle = cardTemplate.content.querySelector('.card__title');
+  const cardImage = cardTemplate.content.querySelector('.card__image');
+
+  // 2.Наполнение карточки данными
   cardTitle.textContent = obj.name;
   cardImage.src = obj.link;
+  cardImage.alt = `Изображение: ${obj.name}`;
+
+  // 3. Создание карточки с функцией удаления
   const card = cardTemplate.content.cloneNode(true);
+  funcRemove(card);
   return card;
 };
 
 // @todo: Функция удаления карточки
+
 const deleteCard = (card) => {
-  card
-    .querySelector('.card__delete-button')
-    .addEventListener('click', (e) => e.target.closest('.places__item').remove());
+  // 1. Получение данных из DOM
+  const removeBtn = card.querySelector('.card__delete-button');
+  const removeItem = card.querySelector('.places__item');
+
+  // 2. Обработчик события
+  removeBtn.addEventListener('click', () => removeItem.remove());
 };
 
 // @todo: Вывести карточки на страницу
-
 initialCards.map((obj) => {
-  const card = createCard(obj);
-  deleteCard(card);
+  const card = createCard(obj, deleteCard);
   placesList.append(card);
 });
