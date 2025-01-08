fe3d8f6ffafcbd0add5c15e0c95a1c82658ed5a8
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-01-08 20:45:20 +0300

    '1.0'

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+const cardTitle = cardTemplate.content.querySelector('.card__title');
+const cardImage = cardTemplate.content.querySelector('.card__image');
 
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
+const deleteCard = (card) => {
+  card
+    .querySelector('.card__delete-button')
+    .addEventListener('click', (e) => e.target.closest('.places__item').remove());
+};
 
 // @todo: Вывести карточки на страницу
+
+initialCards.map((i) => {
+  cardTitle.textContent = i.name;
+  cardImage.src = i.link;
+  let card = cardTemplate.content.cloneNode(true);
+  deleteCard(card);
+  placesList.append(card);
+});
