9f137cef2eea98353c506cd7cde1e7ae95607f8a
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-01-08 20:56:52 +0300

    1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,18 @@
 // @todo: Темплейт карточки
-const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 const cardTitle = cardTemplate.content.querySelector('.card__title');
 const cardImage = cardTemplate.content.querySelector('.card__image');
 
 // @todo: Функция создания карточки
+const createCard = (obj) => {
+  cardTitle.textContent = obj.name;
+  cardImage.src = obj.link;
+  const card = cardTemplate.content.cloneNode(true);
+  return card;
+};
 
 // @todo: Функция удаления карточки
 const deleteCard = (card) => {
@@ -17,10 +23,8 @@ const deleteCard = (card) => {
 
 // @todo: Вывести карточки на страницу
 
-initialCards.map((i) => {
-  cardTitle.textContent = i.name;
-  cardImage.src = i.link;
-  let card = cardTemplate.content.cloneNode(true);
+initialCards.map((obj) => {
+  const card = createCard(obj);
   deleteCard(card);
   placesList.append(card);
 });
