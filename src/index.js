3d64e99b85b63cc0f2b722c1c543d776b04f14e4
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-02-27 00:18:12 +0300

    4.2.5.0 - фикс ошибки валидации

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ const validationConfig = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
+  inactiveButtonClass: 'popup__button_inactive',
+  inputErrorClass: 'popup__input_error',
   errorClass: 'popup__input-error_active',
 };
 
